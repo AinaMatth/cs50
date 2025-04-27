9e0641e02b849c00db59a31cb4611c23bfc16fb6
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-27 18:57:36 +0300

    feat: solution

--- lecture-1/credit/cash.c
+++ lecture-1/credit/cash.c
@@ -1,15 +1,32 @@
-// TODO: Check CS50 problem
 #include <cs50.h>
 #include <stdio.h>
 int get_change(void);
+int calc_coins(int money);
 int main(void) {
   int money = get_change();
-  printf("The cashier own you: %i\n", money);
+  int coins = calc_coins(money);
+  printf("%i\n", coins);
 }
 int get_change(void) {
   int cents;
   do {
-    cents = get_int("Change owned in cents: ");
-  } while (cents <= 0 || cents >= 100);
+    cents = get_int("Change owned: ");
+  } while (cents <= 0);
   return cents;
+}
+int calc_coins(int money) {
+  const int quarter = 25;
+  const int dime = 10;
+  const int nickel = 5;
+  const int penny = 1;
+  int coins = 0;
+  coins += money / quarter;
+  money %= quarter;
+  coins += money / dime;
+  money %= dime;
+  coins += money / nickel;
+  money %= nickel;
+  coins += money / penny;
+  money %= penny;
+  return coins;
 }
